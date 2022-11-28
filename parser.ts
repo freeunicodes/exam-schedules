5df03d316704a95b4b5d2eb6e36c0a77f3dc0fcc
Luka Nadiradze <lnadi17@freeuni.edu.ge>
2022-11-29 00:12:25 +0400

    Reformat parser.ts

--- parser.ts
+++ parser.ts
@@ -1,43 +1,43 @@
 const {google} = require('googleapis');
-const {authorize} = require("./index");
-const fs =require("fs");
+const {authorize} = require("./index.ts");
+const fs = require("fs");
 
 interface ExamInfo {
-    date : string,
-    time : string,
-    subject : string,
-    lecturers : string,
-    groups : string,
-    university : string
+    date: string,
+    time: string,
+    subject: string,
+    lecturers: string,
+    groups: string,
+    university: string
 }
 
-const getSheets = async (sheets : any, spreadsheetId : any) => {
+const getSheets = async (sheets: any, spreadsheetId: any) => {
     let result = (await sheets.spreadsheets.get({
         spreadsheetId
     })).data.sheets;
-    result = result.map((sheet : any) => {
+    result = result.map((sheet: any) => {
         return sheet.properties.title
     })
     return result
 }
 
 
-function getSpreadsheetId() : string{
-    if (!fs.existsSync(  "./data/SpreadsheetId.json")) {
+function getSpreadsheetId(): string {
+    if (!fs.existsSync("./data/SpreadsheetId.json")) {
         throw new Error(`SpreadsheetId.json not found in ./data/ directory`);
     }
     return JSON.parse(fs.readFileSync("./data/SpreadsheetId.json").toString()).spreadsheetId;
 }
 
-async function listMajors(auth : any) {
+async function listMajors(auth: any) {
     const sheets = google.sheets({version: 'v4', auth});
-    let ranges : string[] = [];
+    let ranges: string[] = [];
     await getSheets(sheets, getSpreadsheetId())
-        .then((row : any) => {
-            ranges=row;
+        .then((row: any) => {
+            ranges = row;
         });
 
-    ranges.map(async (range : string) => {
+    ranges.map(async (range: string) => {
         const res = await sheets.spreadsheets.values.get({
             spreadsheetId: getSpreadsheetId(),
             range: `${range}!A2:G`,
@@ -47,14 +47,14 @@ async function listMajors(auth : any) {
             console.log('No data found.');
             return;
         }
-        rows.forEach((row : any) => {
-            let examInfo : ExamInfo = {
-                date : range,
-                time : row[0],
-                subject : row[1],
-                lecturers : row[2],
-                groups : row[3],
-                university : row[4]
+        rows.forEach((row: any) => {
+            let examInfo: ExamInfo = {
+                date: range,
+                time: row[0],
+                subject: row[1],
+                lecturers: row[2],
+                groups: row[3],
+                university: row[4]
             }
             console.log(examInfo);
         });
