d3e9d51e2ab0eb4635eb5c88a4fec06e72185d8a
Luka Nadiradze <lnadi17@freeuni.edu.ge>
2022-12-08 05:52:58 +0400

    Add initial friendly tests

--- src/filters.ts
+++ src/filters.ts
@@ -1,35 +1,34 @@
 import {ExamInfo} from "./interfaces/ExamInfo";
 
-//function returns true if this lecturer is owner of this exam
+// Function returns true if this lecturer is owner of this exam
+// TODO: It would be better if all this was implemented with regex (at least make it better or tests will start to fail)
 function containsLecturer(exam: ExamInfo, lecturer: string) {
     let lecturerNameFirstLetter = lecturer[0];
-    let lecturerSign = lecturerNameFirstLetter + " " + lecturer.split(" ")[1];
+    let lecturerLastName = lecturer.split(" ")[1];
+    let lecturerSign = lecturerNameFirstLetter + " " + lecturerLastName;
     let lecturerArray = exam.lecturers;
-    return lecturerArray.includes(lecturer) || lecturerArray.includes(lecturerSign);
+    let containsSignature = lecturerArray.some(el => el[0] === lecturerNameFirstLetter && el.split(" ")[1] === lecturerLastName);
+    return lecturerArray.includes(lecturer) || lecturerArray.includes(lecturerSign) || containsSignature;
 }
 
-//function returns filter array of examlist
 function byLecturer(examsList: ExamInfo[], lecturer: string) {
     return examsList.filter((exam: ExamInfo) => containsLecturer(exam, lecturer));
 }
 
-//function returns true if this group is one of the groups of the exam
+// Function returns true if this group is one of the groups of the exam
 function containsGroup(exam: ExamInfo, group: string) {
     let groupsArray = exam.groups;
     return groupsArray.includes(group);
 }
 
-//function returns filter array of examlist
 function byGroup(examsList: ExamInfo[], group: string) {
     return examsList.filter((exam: ExamInfo) => containsGroup(exam, group));
 }
 
-//function returns filter array of examlist
 function bySubject(examsList: ExamInfo[], subject: string) {
     return examsList.filter((exam: ExamInfo) => exam.subject.includes(subject));
 }
 
-//function returns filter array of examlist
 function byUniversity(examsList: ExamInfo[], university: string) {
     return examsList.filter((exam: ExamInfo) => exam.university === university);
 }
--- test/filters.test.ts
+++ test/filters.test.ts
@@ -1,14 +1,37 @@
-import { expect } from 'chai'
+import {expect} from 'chai'
 import {ExamInfo} from "../src/interfaces/ExamInfo";
 import filters from "../src/filters";
 
 describe(`filtering exams`, () => {
-    describe(`byLecturer`, () => {
-        it(`should find lecture by full first name`, () => {
-            // ...
+    const examInfo: ExamInfo = {
+        date: "",
+        groups: [],
+        subject: "",
+        time: "",
+        university: "",
+        lecturers: ["ია მღვდლიაშვილი"]
+    }
+
+    const examInfoSignature: ExamInfo = {
+        ...examInfo,
+        lecturers: ["ი. მღვდლიაშვილი"]
+    }
+
+    describe(`byLecturer (ია მღვდლიაშვილი)`, () => {
+        it(`should find lecture by lecturer's full name`, () => {
+            expect(filters.byLecturer([examInfo], "ია მღვდლიაშვილი")).to.include(examInfo);
         })
-        it(`should find lecture by initial of first name`, () => {
-            // ...
+        it(`should find lecture by lecturer's signature`, () => {
+            expect(filters.byLecturer([examInfo], "ი. მღვდლიაშვილი")).to.include(examInfo);
+        })
+    })
+
+    describe(`byLecturer (ი. მღვდლიაშვილი)`, () => {
+        it(`should find lecture by lecturer's full name`, () => {
+            expect(filters.byLecturer([examInfoSignature], "ია მღვდლიაშვილი")).to.include(examInfoSignature);
+        })
+        it(`should find lecture by lecturer's signature`, () => {
+            expect(filters.byLecturer([examInfoSignature], "ი. მღვდლიაშვილი")).to.include(examInfoSignature);
         })
     })
 })
