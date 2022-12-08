5a126931c6aa22c2342f20cb64d0290a7f10daae
Luka Nadiradze <lnadi17@freeuni.edu.ge>
2022-12-08 05:19:32 +0400

    Add mocha/chai and test template

---
+++ test/filters.test.ts
@@ -0,0 +1,14 @@
+import { expect } from 'chai'
+import {ExamInfo} from "../src/interfaces/ExamInfo";
+import filters from "../src/filters";
+
+describe(`filtering exams`, () => {
+    describe(`byLecturer`, () => {
+        it(`should find lecture by full first name`, () => {
+            // ...
+        })
+        it(`should find lecture by initial of first name`, () => {
+            // ...
+        })
+    })
+})
