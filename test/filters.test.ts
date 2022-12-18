0e9d460e75b68d0f3b32a8cd7095da5ec7f1a782
AkakiGurgenidze <agurg21@freeuni.edu.ge>
2022-12-19 00:01:28 +0400

    Change tests style

--- test/filters.test.ts
+++ test/filters.test.ts
@@ -21,79 +21,76 @@ describe(`filtering exams`, () => {
         university: 'Freeuni'
     }
 
-    function testFilterFunction(filterFn : any, examInfo: any, searchString: string, shouldInclude: boolean){
-        if(shouldInclude)
-            expect(filterFn([examInfo], searchString).map((exam:any) => exam.searchExam)).to.include(examInfo)
+    function testFilterFunction(filterFn: any, examInfo: any, searchString: string, shouldInclude: boolean) {
+        if (shouldInclude)
+            expect(filterFn([examInfo], searchString).map((exam: any) => exam.searchExam)).to.include(examInfo)
         else
-            expect(filterFn([examInfo], searchString).map((exam:any) => exam.searchExam)).to.not.include(examInfo)
+            expect(filterFn([examInfo], searchString).map((exam: any) => exam.searchExam)).to.not.include(examInfo)
     }
 
-    //TODO
     describe(`byLecturer should find exam by lecturer name`, () => {
         it(`full name in search query, full name in schedule`, () => {
-            testFilterFunction(filters.byLecturer,examInfo,"ია მღვდლიაშვილი",true)
-            //expect(filters.byLecturer([examInfo], "")).to.include(examInfo);
+            testFilterFunction(filters.byLecturer, examInfo, "ია მღვდლიაშვილი", true)
         })
         it(`signature in search query, full name in schedule`, () => {
-            expect(filters.byLecturer([examInfo], "ი. მღვდლიაშვილი")).to.include(examInfo);
+            testFilterFunction(filters.byLecturer, examInfo, "ი. მღვდლიაშვილი", true)
         })
         it(`full name in search query, signature in schedule`, () => {
-            expect(filters.byLecturer([examInfoSignature], "ზაზა ოსმანოვი")).to.include(examInfoSignature);
+            testFilterFunction(filters.byLecturer, examInfoSignature, "ზაზა ოსმანოვი", true)
         })
         it(`signature in search query, signature in schedule`, () => {
-            expect(filters.byLecturer([examInfoSignature], "ზ. ოსმანოვი")).to.include(examInfoSignature);
+            testFilterFunction(filters.byLecturer, examInfoSignature, "ზ. ოსმანოვი", true)
         })
         it(`first name in search query, full name in schedule`, () => {
-            expect(filters.byLecturer([examInfo], "ია")).to.include(examInfo);
+            testFilterFunction(filters.byLecturer, examInfo, "ია", true)
         })
         it(`substring in search query, full name in schedule`, () => {
-            expect(filters.byLecturer([examInfo], "მღვდლ")).to.include(examInfo);
+            testFilterFunction(filters.byLecturer, examInfo, "მღვდლ", true)
         })
         it(`substring in search query, signature in schedule`, () => {
-            expect(filters.byLecturer([examInfoSignature], "ოსმა")).to.include(examInfoSignature);
+            testFilterFunction(filters.byLecturer, examInfoSignature, "ოსმა", true)
         })
         it(`wrong name in search query, full name in schedule`, () => {
-            expect(filters.byLecturer([examInfo], "ნიკა")).to.not.include(examInfo);
+            testFilterFunction(filters.byLecturer, examInfo, "ნიკა", false)
         })
         it(`wrong name in search query, signature in schedule`, () => {
-            testFilterFunction(filters.byLecturer,examInfoSignature,"ნიკა",false)
-            //expect(filters.byLecturer([examInfoSignature], "ნიკა")).to.not.include(examInfoSignature);
+            testFilterFunction(filters.byLecturer, examInfoSignature, "ნიკა", false)
         })
     })
 
     describe(`bySubject should find exam by subject name`, () => {
         it(`full name in search query`, () => {
-            expect(filters.bySubject([examInfo], "შესავალი ციფრულ ტექნოლოგიებში")).to.include(examInfo);
+            testFilterFunction(filters.bySubject, examInfo, "შესავალი ციფრულ ტექნოლოგიებში", true)
         })
         it(`single word in search query`, () => {
-            expect(filters.bySubject([examInfo], "შესავალი")).to.include(examInfo);
+            testFilterFunction(filters.bySubject, examInfo, "შესავალი", true)
         })
         it(`typo in search query`, () => {
-            expect(filters.bySubject([examInfo], "შესქვსალი ცფრულ ტექოლოგიბში")).to.include(examInfo);
+            testFilterFunction(filters.bySubject, examInfo, "შესქვსალი ცფრულ ტექოლოგიბში", true)
         })
         it(`substring in search query`, () => {
-            expect(filters.bySubject([examInfo], "ციფრ")).to.include(examInfo);
+            testFilterFunction(filters.bySubject, examInfo, "ციფრ", true)
         })
         it(`typo in substring in search query`, () => {
-            expect(filters.bySubject([examInfo], "ტეხნოლოგიები")).to.include(examInfo);
+            testFilterFunction(filters.bySubject, examInfo, "ტეხნოლოგიები", true)
         })
         it(`other subject name in search query`, () => {
-            expect(filters.bySubject([examInfo], "კალკულუსი")).to.not.include(examInfo);
+            testFilterFunction(filters.bySubject, examInfo, "კალკულუსი", false)
         })
     })
 
     describe(`byUniversity should find exam by university name`, () => {
         it(`full university name in search query`, () => {
-            expect(filters.byUniversity([examInfo], "Freeuni")).to.include(examInfo);
+            testFilterFunction(filters.byUniversity, examInfo, "Freeuni", true)
         })
         it(`typo in search query`, () => {
-            expect(filters.byUniversity([examInfo], "freeumi")).to.include(examInfo);
+            testFilterFunction(filters.byUniversity, examInfo, "freeumi", true)
         })
         it(`substring in search query`, () => {
-            expect(filters.byUniversity([examInfo], "fre")).to.include(examInfo);
+            testFilterFunction(filters.byUniversity, examInfo, "fre", true)
         })
         it(`other university name in search query`, () => {
-            expect(filters.byUniversity([examInfo], "Agruni")).to.not.include(examInfo);
+            testFilterFunction(filters.byUniversity, examInfo, "Agruni", false)
         })
     })
 })
