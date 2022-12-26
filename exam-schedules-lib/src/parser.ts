f88ca9d59e8b20bd44a73cab2d0ca4a2d2482ffc
AkakiGurgenidze <agurg21@freeuni.edu.ge>
2022-12-27 01:20:39 +0400

    Add batch request to send all requests together

--- exam-schedules-lib/src/parser.ts
+++ exam-schedules-lib/src/parser.ts
@@ -2,7 +2,7 @@ const {google} = require('googleapis');
 const fs = require("fs");
 import {ExamInfo} from "./interfaces/ExamInfo";
 
-const fetchFullData = false
+const spreadsheetdIdPath = "../data/SpreadsheetId.json"
 
 //return array of Spreadsheets' titles
 const getExamDates = async (sheets: any, spreadsheetId: any) => {
@@ -10,16 +10,13 @@ const getExamDates = async (sheets: any, spreadsheetId: any) => {
         spreadsheetId
     })).data.sheets;
     result = result.map((sheet: any) => {
-        return sheet.properties.title
+        return `${sheet.properties.title}!A2:G`
     })
-    // TODO
-    if (!fetchFullData) result = result.slice(0, 10);
     return result;
 }
 
 // Get spreadsheet ID from file
 function getSpreadsheetId(): string {
-    let spreadsheetdIdPath = "../data/SpreadsheetId.json"
     if (!fs.existsSync(spreadsheetdIdPath)) {
         throw new Error(`SpreadsheetId.json not found in ../data/ directory`);
     }
@@ -27,56 +24,52 @@ function getSpreadsheetId(): string {
 }
 
 // Returns array of examInfo for this date
-async function getExamListForDate(range: string, sheets: any) {
-    // This should be other function
-    const res = await sheets.spreadsheets.values.get({
+async function getExamListForDate(ranges: string[], sheets: any, authClient: any) {
+    const request = {
         spreadsheetId: getSpreadsheetId(),
-        range: `${range}!A2:G`,
-    });
-    const rows = res.data.values;
+        ranges: ranges,
+        auth: authClient,
+    };
 
-    if (!rows || rows.length === 0) {
-        return undefined;
-    }
-    return rows.map((row: any) => {
-        if (row.length < 5 || row.includes(undefined)) return undefined;
-        let lecturersArray = row[2].split(",");
-        lecturersArray = lecturersArray.map((lecturer: string) => {
-            return lecturer.split(".").join(" ").split("  ").join(" ").trim();
-        });
-        let groupsArray = row[3].split(",");
-        groupsArray = groupsArray.map((group: string) => {
-            return group.trim();
+    try {
+        const response = (await sheets.spreadsheets.values.batchGet(request)).data;
+        return response.valueRanges.map((currRangeRes: any) => {
+            const currRange = currRangeRes.range.substring(0, 6)
+            const rows = currRangeRes.values
+            if (!rows || rows.length === 0) {
+                return undefined;
+            }
+            return rows.map((row: any) => {
+                if (row.length < 5 || row.includes(undefined)) return undefined;
+                let lecturersArray = row[2].split(",");
+                lecturersArray = lecturersArray.map((lecturer: string) => {
+                    return lecturer.split(".").join(" ").split("  ").join(" ").trim();
+                });
+                let groupsArray = row[3].split(",");
+                groupsArray = groupsArray.map((group: string) => {
+                    return group.trim();
+                })
+                let examInfo: ExamInfo = {
+                    date: currRange,
+                    time: row[0],
+                    subject: row[1],
+                    lecturers: lecturersArray,
+                    groups: groupsArray,
+                    university: row[4]
+                }
+                return examInfo;
+            });
         })
-        let examInfo: ExamInfo = {
-            date: range,
-            time: row[0],
-            subject: row[1],
-            lecturers: lecturersArray,
-            groups: groupsArray,
-            university: row[4]
-        }
-        return examInfo;
-    });
-}
-
-function sleep(delay: number) {
-    let start = new Date().getTime();
-    while (new Date().getTime() < start + delay) {}
+    } catch (err: any) {
+        throw new Error(err!.title)
+    }
 }
 
 // Returns array of all exams
 export async function getExamList(auth: any) {
     const sheets = google.sheets({version: 'v4', auth})
     const ranges: string[] = await getExamDates(sheets, getSpreadsheetId())
-
-    let rangeCnt = 0;
-    const promises = ranges.map(range => {
-        rangeCnt++;
-        if (fetchFullData && rangeCnt % 50 == 0) sleep(60 * 1000)
-        return getExamListForDate(range, sheets)
-    })
+    const promises = await getExamListForDate(ranges, sheets, auth)
     let result = (await Promise.all(promises)).flat()
-    result = result.filter((exam: any) => exam != undefined);
-    return result;
+    return result.filter((exam: any) => exam != undefined);
 }
