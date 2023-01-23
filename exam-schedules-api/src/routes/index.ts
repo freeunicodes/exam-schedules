bf0160e6ec7852ec7d5d2088b542652cbcc74939
Luka Nadiradze <lnadi17@freeuni.edu.ge>
2023-01-23 17:24:49 +0400

    Add basic API tests

--- exam-schedules-api/src/routes/index.ts
+++ exam-schedules-api/src/routes/index.ts
@@ -20,23 +20,44 @@ export function getCachedState(): State {
     return fetchInfo;
 }
 
-indexRouter.use((req: Request, res: Response, next: NextFunction) => {
+let nowFetching: boolean = false
+
+indexRouter.use(async (req: Request, res: Response, next: NextFunction) => {
+    // Wait if request is already being made
+    await until(nowFetching)
+
     if (fetchInfo.lastFetchTime === undefined || (Date.now() - fetchInfo.lastFetchTime) > delay) {
-        console.log("Now fetching")
+        console.log("Now fetching");
+        nowFetching = true;
         authAndGetData()
             .then((response: ExamInfo[]) => {
                 fetchInfo.examsList = response
                 fetchInfo.lastFetchTime = Date.now()
                 next()
             })
-            .catch(err => next(err));
+            .catch(err => {
+                next(err)
+            })
+            .finally(() => {
+                nowFetching = false
+            })
     } else {
         next()
     }
 })
 
+
 indexRouter.get('/', function (req: Request, res: Response) {
     res.send(fetchInfo);
 })
+
+function until(condition: boolean) {
+    const poll = (resolve: (value?: {} | PromiseLike<{}>) => void) => {
+        if (!condition) resolve();
+        else setTimeout(_ => poll(resolve), 400);
+    }
+
+    return new Promise(poll);
+}
 
 
--- exam-schedules-api/test/api.test.ts
+++ exam-schedules-api/test/api.test.ts
@@ -2,17 +2,46 @@ import chaiHttp from 'chai-http';
 import app from '../src/app'
 import {expect} from "chai";
 import chai from 'chai'
+import {Response} from "express";
 
+chai.use(chaiHttp)
 
-describe('Test Api', function () {
-    chai.use(chaiHttp)
-    it('should return 200', function () {
-        chai.request(app)
-            .get('/')
-            .end((err, res) => {
-                expect(err).to.be.null
-                expect(res).to.have.status(200)
-            })
-    });
-
+describe('test ok (200) statuses', function () {
+    it('/ should return 200', (done) => {
+        expectStatus(chai.request(app).get('/'), 200, done)
+    })
+    it('/filters/ should return 200', (done) => {
+        expectStatus(chai.request(app).get('/filters/'), 200, done);
+    })
+    it('/filters/lecturer/test should return 200', (done) => {
+        expectStatus(chai.request(app).get('/filters/lecturer/test'), 200, done)
+    })
+    it('/filters/subject/test should return 200', (done) => {
+        expectStatus(chai.request(app).get('/filters/subject/test'), 200, done)
+    })
+    it('/filters/university/test should return 200', (done) => {
+        expectStatus(chai.request(app).get('/filters/university/test'), 200, done)
+    })
 });
+
+describe('test response bodies', function () {
+    it('/ should have lastFetchTime', (done) => {
+        chai.request(app).get('/').end((err, res) => {
+            expect(res.body).to.haveOwnProperty("lastFetchTime")
+            done()
+        })
+    })
+    it('/ should have examsList', (done) => {
+        chai.request(app).get('/').end((err, res) => {
+            expect(res.body).to.haveOwnProperty("examsList")
+            done()
+        })
+    })
+})
+
+function expectStatus(request: any, status: number, done: Mocha.Done) {
+    request.end((err: any, res: any) => {
+        expect(res).to.have.status(status)
+        done()
+    })
+}
